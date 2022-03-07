2334e4bdba78932336fd740178a38bd82a5fb1b5
Baysarov <adambajsarov@gmail.com>
2022-03-07 16:16:11 +0300

    patch

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const getBtn = document.getElementById('getBtn')
 const output = document.getElementById('output')
+fetch('https://api.chucknorris.io/jokes/random')
+.then((res) => res.json())
+.then((data) => output.innerHTML = data.value)
+
 getBtn.addEventListener('click', () => {
     fetch('https://api.chucknorris.io/jokes/random')
     .then((res) => res.json())
